e50dac4ac59ebe2762dfa0dfb9aa5d1843f50604
nature_min <russoll3579@gmail.com>
2025-09-13 20:38:56 +0900

    i2c 초기 설정 함수 추가

---
+++ Dry_Rack/i2c.c
@@ -0,0 +1,33 @@
+﻿/*
+ * i2c.c
+ *
+ * Created: 09-13 (토) 오후 8:24:14
+ *  Author: csm
+ */ 
+#include "i2c.h"
+
+// 100kHz SCL 주파수 설정 (F_CPU = 16MHz)
+void i2c_init(void)
+{
+	TWSR = 0x00; // 프리스케일러 1
+	TWBR = 72;   // Bit Rate 설정
+	TWCR = (1 << TWEN);
+}
+
+void i2c_start(void)
+{
+	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+	while (!(TWCR & (1 << TWINT)));
+}
+
+void i2c_stop(void)
+{
+	TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
+}
+
+void i2c_write(uint8_t data)
+{
+	TWDR = data;
+	TWCR = (1 << TWINT) | (1 << TWEN);
+	while (!(TWCR & (1 << TWINT)));
+}
--- Dry_Rack/main.c
+++ Dry_Rack/main.c
@@ -9,6 +9,11 @@
 #include "usart.h"
 #include "timer.h" 
 
+// --- 예약 모드 시간 설정 (밀리초 단위) ---
+const unsigned long FAN_ON_DURATION_MS      = 3UL * 60 * 1000;  // 팬 작동 시간: 3분
+const unsigned long FAN_OFF_INTERVAL_MS     = 27UL * 60 * 1000; // 팬 휴식 시간: 27분
+const unsigned long TOTAL_SCHEDULE_TIME_MS  = 1UL * 60 * 60 * 1000; // 기본 시간: 1시간
+
 int main(void)
 {
     /* Replace with your application code */
@@ -17,7 +22,7 @@ int main(void)
     USART_init();
 	timer_init();
 	
-	 sei(); // 모든 초기화가 끝난 후 전역 인터럽트 활성화
+	sei(); // 모든 초기화가 끝난 후 전역 인터럽트 활성화
 
     while (1) 
     {
