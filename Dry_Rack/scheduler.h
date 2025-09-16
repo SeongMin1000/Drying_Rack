9276838428cbf82195651168040c82d5f957cfd2
nature_min <russoll3579@gmail.com>
2025-09-16 15:25:23 +0900

    예약 타이머 함수 추가

---
+++ Dry_Rack/scheduler.h
@@ -0,0 +1,19 @@
+﻿/*
+ * scheduler.h
+ *
+ * Created: 09-16 (화) 오후 3:22:09
+ *  Author: csm
+ */ 
+
+
+#ifndef SCHEDULER_H_
+#define SCHEDULER_H_
+
+#include <avr/io.h>
+
+void reservation_start(uint8_t hours);
+void reservation_stop(void);
+uint8_t reservation_is_active(void);
+uint8_t reservation_is_finished(void);
+
+#endif /* SCHEDULER_H_ */
